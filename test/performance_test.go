a99043c4586f18587f2dab410559bef55fcf350a
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-23 03:01:16 +0600

    Create performance_test.go

---
+++ test/performance_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"testing"
+
+	"github.com/khulnasoft-lab/vul-iac/pkg/scanners/terraform/executor"
+	"github.com/khulnasoft-lab/vul-iac/pkg/scanners/terraform/parser"
+	"github.com/khulnasoft-lab/vul-iac/test/testutil"
+	"github.com/khulnasoft-lab/vul-policies/pkg/rules"
+)
+
+func BenchmarkCalculate(b *testing.B) {
+
+	f, err := createBadBlocks()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p := parser.New(f, "", parser.OptionStopOnHCLError(true))
+		if err := p.ParseFS(context.TODO(), "project"); err != nil {
+			b.Fatal(err)
+		}
+		modules, _, err := p.EvaluateAll(context.TODO())
+		if err != nil {
+			b.Fatal(err)
+		}
+		_, _, _ = executor.New().Execute(modules)
+	}
+}
+
+func createBadBlocks() (fs.FS, error) {
+
+	files := make(map[string]string)
+
+	files["/project/main.tf"] = `
+module "something" {
+	source = "../modules/problem"
+}
+`
+
+	for _, rule := range rules.GetRegistered() {
+		if rule.GetRule().Terraform == nil {
+			continue
+		}
+		for i, bad := range rule.GetRule().Terraform.BadExamples {
+			filename := fmt.Sprintf("/modules/problem/%s-%d.tf", rule.GetRule().LongID(), i)
+			files[filename] = bad
+		}
+	}
+
+	f := testutil.CreateFS(&testing.T{}, files)
+	return f, nil
+}
